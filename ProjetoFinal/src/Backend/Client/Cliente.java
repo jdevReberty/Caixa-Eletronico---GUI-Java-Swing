34c755be1cb7ae399ae13262d8b66bfa37be68fd
jdevReberty <jdev.contato@gmail.com>
2023-02-01 19:51:30 -0300

    interface de usuarios e classe de cliente

---
+++ ProjetoFinal/src/Backend/Client/Cliente.java
@@ -0,0 +1,56 @@
+package Backend.Client;
+
+import Backend.ContaCorrente;
+import Backend.ContaPoupanca;
+
+public class Cliente implements Usuarios {
+    
+    private ContaCorrente conta_corrente;
+    private ContaPoupanca conta_poupanca;
+
+    private String nome;
+    private String cpf;
+    private String data_nascimento;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public void setDataNascimento(String data_nascimento){
+        this.data_nascimento = data_nascimento;
+    }
+    public String getDataNascimento(){
+        return this.data_nascimento;
+    }
+
+    @Override
+    public void cadastrarUsuario(String nome, String cpf, String data_nascimento, double saldo, String senha) {
+        this.setNome(nome);
+        this.setCpf(cpf);
+        this.setDataNascimento(data_nascimento);
+
+        cadastrarConta(saldo, "sdusdu", senha);
+    }
+
+    public void cadastrarConta(double saldo, String conta, String senha) {
+        implementarContaCorrente(saldo, conta, senha);
+        implementarContaPoupanca(saldo, conta, senha);
+    }
+
+    public void implementarContaCorrente(double saldo, String conta, String senha) {
+        this.conta_corrente = new ContaCorrente(saldo, conta, senha);
+    }
+    public void implementarContaPoupanca(double saldo, String conta, String senha) {
+        this.conta_poupanca = new ContaPoupanca(saldo, conta, senha);
+    }
+}
---
+++ ProjetoFinal/src/Backend/Client/Usuarios.java
@@ -0,0 +1,7 @@
+package Backend.Client;
+
+public interface Usuarios {
+    
+    public void cadastrarUsuario(String nome, String cpf, String data_nascimento, double saldo, String senha);
+
+}
