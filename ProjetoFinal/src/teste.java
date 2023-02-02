94ef21f4d1e0dbd40dd4b814f4de60db95088f10
jdevReberty <jdev.contato@gmail.com>
2023-02-01 21:52:47 -0300

    metodo de teste

---
+++ ProjetoFinal/src/teste.java
@@ -0,0 +1,33 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Backend.Transacao;
+import Backend.Client.Cliente;
+public class teste {
+    
+    public static void main(String[] args) {
+        
+        List<Cliente> listaCliente = Cliente.criarVetorCliente();
+        Transacao servico_transacao = new Transacao();
+
+        listaCliente.stream().forEach(cliente -> {
+            System.out.println(cliente.getContaCorrente().getSaldo());
+            System.out.println(cliente.getNome());
+            System.out.println("---------------------------");
+        });
+
+        listaCliente.stream().forEach(cliente -> {
+            servico_transacao.depositar(listaCliente, cliente.getContaCorrente().getConta(), 200, 1);
+        });
+
+        listaCliente.stream().forEach(cliente -> {
+            System.out.println(cliente.getContaCorrente().getSaldo());
+            System.out.println(cliente.getNome());
+            System.out.println("---------------------------");
+        });
+
+
+    }
+}
